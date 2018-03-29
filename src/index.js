b7cb3bf06b043aef3304c029ca724411e32e058d
cherish <xieqin1991@gmail.com>
2018-03-29 14:59:41 +0800

    add animation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('hello world')
+import { tween } from 'popmotion'
+import './index.css'
+
+const counter = document.querySelector('#count')
+const updateCounter = (v) => counter.innerHTML = (v - 0).toFixed(2)
+
+const counterButton = document.querySelector('#counter')
+
+counterButton.addEventListener('click', () => {
+    tween({
+        to: 1000,
+        duration: 1000
+    }).start(updateCounter)
+})
+
+
