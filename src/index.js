dfbc81230bfe892a361c6ab5b37fc7baecdb066c
cherish <xieqin1991@gmail.com>
2018-03-29 16:10:31 +0800

    add action

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import { tween } from 'popmotion'
+import {
+    tween,
+    styler,
+    easing,
+    action
+} from 'popmotion'
 import './index.css'
 
 const counter = document.querySelector('#count')
 const updateCounter = (v) => counter.innerHTML = (v - 0).toFixed(2)
-
 const counterButton = document.querySelector('#counter')
 
 counterButton.addEventListener('click', () => {
@@ -13,4 +17,33 @@ counterButton.addEventListener('click', () => {
     }).start(updateCounter)
 })
 
+const ball = document.querySelector('#ball')
+const baller = document.querySelector('#baller')
+const ballStyler = styler(ball)
+
+baller.addEventListener('click', () => {
+    tween({
+        from: {
+            x: 0,
+            scale: 1
+        },
+        to: {
+            x: 150,
+            scale: 2
+        },
+        ease: easing.easeInOut,
+        flip: Infinity,
+        duration: 2000
+    }).start(ballStyler.set)
+})
+
+const just = (v) => action(({ update, complete }) => {
+    update(v)
+    complete()
+})
+
+just(1).start({
+    update: console.log,
+    complete: () => console.log('completed!')
+})
 
